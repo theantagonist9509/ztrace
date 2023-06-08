f82c56b4784d9299c3c8a169ebe0ce54278c6014
* <tejas.tanmay595@gmail.com>
2023-06-08 14:08:10 +0530

    Cleaned up main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,5 @@
 const std = @import("std");
 
-const Ppm = @import("ppm.zig").Ppm;
-const Sphere = @import("sphere.zig").Sphere;
 const World = @import("world.zig").World;
 
 pub fn main() !void {
@@ -11,5 +9,5 @@ pub fn main() !void {
     var world = try World.initFromJson(arena.allocator(), "example.json");
 
     try world.raytrace(arena.allocator(), 1, 100); // TODO cmd line arg parsing
-    try world.camera.image.writeToFile("image.ppm");
+    try world.camera.image.writeToFile("example.ppm");
 }
